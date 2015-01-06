fe47b826890bb00efde421338f6394c4badba817
Paul Guyot <pguyot@kallisys.net>
2015-01-06 07:00:59 +0100

    Try to run CLITest on Travis

--- list_tests_travis.sh
+++ list_tests_travis.sh
@@ -2,27 +2,13 @@
 
 echo "Compiling with xctool"
 
-xctool -project _Build_/Xcode6/Einstein.xcodeproj -scheme Einstein build
+xctool -project _Build_/Xcode6/Einstein.xcodeproj -scheme CLITest build
 
-echo "Invoking otest-query-osx"
+echo "Running CLITest"
 echo "Command is :"
 
-echo OBJC_DISABLE_GC=YES \
-__CFPREFERENCES_AVOID_DAEMON=YES \
-NSUnbufferedIO=YES \
-DYLD_FRAMEWORK_PATH=/Users/travis/Library/Developer/Xcode/DerivedData/Einstein-*/Build/Products/Debug \
-DYLD_FALLBACK_FRAMEWORK_PATH=/Applications/Xcode.app/Contents/Developer/Library/Frameworks:/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks \
-DYLD_LIBRARY_PATH=/Users/travis/Library/Developer/Xcode/DerivedData/Einstein-*/Build/Products/Debug \
-/usr/local/Cellar/xctool/0.2.1/libexec/libexec/otest-query-osx \
-/Users/travis/Library/Developer/Xcode/DerivedData/Einstein-*/Build/Products/Debug/EinsteinTests.xctest
+echo /Users/travis/Library/Developer/Xcode/DerivedData/Einstein-*/Build/Products/Debug/CLITest
 
-OBJC_DISABLE_GC=YES \
-__CFPREFERENCES_AVOID_DAEMON=YES \
-NSUnbufferedIO=YES \
-DYLD_FRAMEWORK_PATH=/Users/travis/Library/Developer/Xcode/DerivedData/Einstein-*/Build/Products/Debug \
-DYLD_FALLBACK_FRAMEWORK_PATH=/Applications/Xcode.app/Contents/Developer/Library/Frameworks:/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks \
-DYLD_LIBRARY_PATH=/Users/travis/Library/Developer/Xcode/DerivedData/Einstein-*/Build/Products/Debug \
-/usr/local/Cellar/xctool/0.2.1/libexec/libexec/otest-query-osx \
-/Users/travis/Library/Developer/Xcode/DerivedData/Einstein-*/Build/Products/Debug/EinsteinTests.xctest
+/Users/travis/Library/Developer/Xcode/DerivedData/Einstein-*/Build/Products/Debug/CLITest
 
-echo "Invoked otest-query-osx"
+echo "Ran CLITest"
